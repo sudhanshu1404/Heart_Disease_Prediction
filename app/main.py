e103c1c029b8891962d0fdc594d932fd2f2ed016
sudhanshu1404 <sudhanshu1404@gmail.com>
2025-07-16 16:25:53 +0530

    Debug True

--- app/main.py
+++ app/main.py
@@ -22,4 +22,4 @@ def predict():
         return render_template('index.html', prediction_text=f"Prediction: {result}")
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
